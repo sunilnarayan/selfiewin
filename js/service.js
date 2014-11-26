2599d09614341bc8c2a4d143688637416ca79ea7
sunilnarayan <naga.sunil3@gmail.com>
2014-11-26 10:47:10 +0530

    Initial Commit

--- js/service.js
+++ js/service.js
@@ -1,6 +1,6 @@
-angular.module('bucketList.services', [])
+angular.module('selfiewin.services', [])
     .factory('API', function ($rootScope, $http, $ionicLoading, $window) {
-       var base = "selfiewin.herokuapp.com";
+       var base = "http://selfiewin.herokuapp.com";
         $rootScope.show = function (text) {
             $rootScope.loading = $ionicLoading.show({
                 content: text ? text : 'Loading',
@@ -10,23 +10,23 @@ angular.module('bucketList.services', [])
                 showDelay: 0
             });
         };
- 
+
         $rootScope.hide = function () {
             $ionicLoading.hide();
         };
- 
+
         $rootScope.logout = function () {
             $rootScope.setToken("");
             $window.location.href = '#/auth/signin';
         };
- 
+
         $rootScope.notify =function(text){
             $rootScope.show(text);
             $window.setTimeout(function () {
               $rootScope.hide();
             }, 1999);
         };
- 
+
         $rootScope.doRefresh = function (tab) {
             if(tab == 1)
                 $rootScope.$broadcast('fetchAll');
@@ -35,28 +35,28 @@ angular.module('bucketList.services', [])
             
             $rootScope.$broadcast('scroll.refreshComplete');
         };
- 
+
         $rootScope.setToken = function (token) {
             return $window.localStorage.token = token;
         }
- 
+
         $rootScope.getToken = function () {
             return $window.localStorage.token;
         }
- 
+
         $rootScope.isSessionActive = function () {
             return $window.localStorage.token ? true : false;
         }
- 
+
         return {
             signin: function (form) {
-                return $http.post(base+'/api/v1/bucketList/auth/login', form);
+                return $http.post(base+'/api/v1/selfiewin/auth/login', form);
             },
             signup: function (form) {
-                return $http.post(base+'/api/v1/bucketList/auth/register', form);
+                return $http.post(base+'/api/v1/selfiewin/auth/register', form);
             },
             getAll: function (email) {
-                return $http.get(base+'/api/v1/bucketList/data/list', {
+                return $http.get(base+'/api/v1/selfiewin/data/list', {
                     method: 'GET',
                     params: {
                         token: email
@@ -64,7 +64,7 @@ angular.module('bucketList.services', [])
                 });
             },
             getOne: function (id, email) {
-                return $http.get(base+'/api/v1/bucketList/data/item/' + id, {
+                return $http.get(base+'/api/v1/selfiewin/data/item/' + id, {
                     method: 'GET',
                     params: {
                         token: email
@@ -72,7 +72,7 @@ angular.module('bucketList.services', [])
                 });
             },
             saveItem: function (form, email) {
-                return $http.post(base+'/api/v1/bucketList/data/item', form, {
+                return $http.post(base+'/api/v1/selfiewin/data/item', form, {
                     method: 'POST',
                     params: {
                         token: email
@@ -80,7 +80,7 @@ angular.module('bucketList.services', [])
                 });
             },
             putItem: function (id, form, email) {
-                return $http.put(base+'/api/v1/bucketList/data/item/' + id, form, {
+                return $http.put(base+'/api/v1/selfiewin/data/item/' + id, form, {
                     method: 'PUT',
                     params: {
                         token: email
@@ -88,7 +88,7 @@ angular.module('bucketList.services', [])
                 });
             },
             deleteItem: function (id, email) {
-                return $http.delete(base+'/api/v1/bucketList/data/item/' + id, {
+                return $http.delete(base+'/api/v1/selfiewin/data/item/' + id, {
                     method: 'DELETE',
                     params: {
                         token: email
